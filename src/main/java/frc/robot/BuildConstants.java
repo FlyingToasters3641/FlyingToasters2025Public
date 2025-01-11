58acbfea3deff951848449e76f702e1e83a60dac
Bravelittletoast <161082878+Bravelittletoast@users.noreply.github.com>
2025-01-11 11:54:27 -0500

    added libraries

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FlyingToasters2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "5ab5dc5c45c4a33325b3e3d5b04205cd300b5cf1";
+  public static final String GIT_DATE = "2025-01-11 11:00:59 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-11 11:54:01 EST";
+  public static final long BUILD_UNIX_TIME = 1736614441184L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
