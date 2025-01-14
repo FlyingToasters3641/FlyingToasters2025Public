0dc1cb69dab66d070d0dd8ed2599dc8289085e61
Mars <74789790+TypicalMars@users.noreply.github.com>
2025-01-13 20:01:28 -0500

    autosims+libaries

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FlyingToasters2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "721c0828c0e5aca710165b35f4d858dd1cd9cee2";
-  public static final String GIT_DATE = "2025-01-11 15:57:07 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-13 18:34:21 EST";
-  public static final long BUILD_UNIX_TIME = 1736811261289L;
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "c19561e6e12cbc1c4abaadc0760f41d686c44572";
+  public static final String GIT_DATE = "2025-01-13 18:36:43 EST";
+  public static final String GIT_BRANCH = "SwerveSimulation";
+  public static final String BUILD_DATE = "2025-01-13 19:53:07 EST";
+  public static final long BUILD_UNIX_TIME = 1736815987776L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
