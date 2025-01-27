f146ce140b0892430f4b666d3b9bdbee2fdf0386
lucas burkowski <lucasburkowski@gmail.com>
2025-01-26 22:58:55 -0500

    WIP intake sim

--- src/main/java/frc/robot/subsystems/intake/IntakeCommands.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeCommands.java
@@ -7,16 +7,4 @@ import edu.wpi.first.wpilibj2.command.Commands;
 
 public class IntakeCommands {
     
-public static Command raiseJoint(Intake m_intake, Double speed) {
-        return Commands.run(() -> m_intake.raiseJointMotor(speed));
-    }
-public static Command lowerJoint(Intake m_intake, Double speed) {
-        return Commands.run(() -> m_intake.lowerJointMotor(speed));
-    }
-    public static Command runWheels(Intake m_intake, double speed) {
-        return Commands.run(() -> m_intake.runWheelRollers(speed));
-    }
-    public static Command reverseWheels(Intake m_intake, double speed) {
-        return Commands.run(() -> m_intake.reverseWheelMotor(speed));
-    }
 }
