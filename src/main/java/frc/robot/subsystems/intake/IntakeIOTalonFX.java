1f26eadbc89858955d06784f7c1909aa232ad4a4
TypicalMars <marsgaaming@gmail.com>
2025-02-04 21:19:38 -0500

    ROBOTMOVEY

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class IntakeIOTalonFX implements IntakeIO {
-    private final String CANbusName = "Lucas";
+    private final String CANbusName = "lucas";
     public final TalonFX IN_JointTalonFX = new TalonFX(2, CANbusName); //This is for the Kraken motor that lowers and raises the intake
     public final TalonFX IN_WheelTalonFX = new TalonFX(1, CANbusName); //This is for the Kraken motor that spins the wheels
 
