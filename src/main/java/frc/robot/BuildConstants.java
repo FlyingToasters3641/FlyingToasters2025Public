56f796a888771491466b3e3c6bf2293e10082d22
Mars <74789790+TypicalMars@users.noreply.github.com>
2025-02-13 19:16:26 -0500

    FIXED recalling the same pose (+ other tree stuff)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FlyingToasters2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 78;
-  public static final String GIT_SHA = "f97db17d9cb27c8096ce3b488b37a736a3223b63";
-  public static final String GIT_DATE = "2025-02-08 12:07:23 EST";
+  public static final int GIT_REVISION = 79;
+  public static final String GIT_SHA = "49ebd52f4bb6f11938b24d9507a3dd8f1678a7f9";
+  public static final String GIT_DATE = "2025-02-12 20:57:21 EST";
   public static final String GIT_BRANCH = "ControlTree";
-  public static final String BUILD_DATE = "2025-02-12 20:07:20 EST";
-  public static final long BUILD_UNIX_TIME = 1739408840196L;
+  public static final String BUILD_DATE = "2025-02-13 18:02:55 EST";
+  public static final long BUILD_UNIX_TIME = 1739487775925L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/lib/BehaviorTree/nodes/DriveToTargetPose.java
+++ src/main/java/frc/robot/lib/BehaviorTree/nodes/DriveToTargetPose.java
@@ -7,21 +7,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.lib.BehaviorTree.Blackboard;
 import frc.robot.lib.BehaviorTree.ExecutionStatus;
+import frc.robot.lib.BehaviorTree.trees.Targets;
 
 public class DriveToTargetPose extends BehaviorTreeNode {
     Command driveToCommand;
     private Pose2d pose;
+    private Targets currentTarget;
 
     public DriveToTargetPose(Blackboard blackboard) {
         super(blackboard);
+        currentTarget = Targets.NONE;
     }
 
     @Override
     public void initialize() {
-        if ((blackboard.getTargetPose("target")) != null) {
+        if ((blackboard.getTargetPose("target")) != null && currentTarget != blackboard.getTarget("target")) {
             pose = blackboard.getTargetPose("target");
+            currentTarget = blackboard.getTarget("target");
             driveToCommand = AutoBuilder.pathfindToPose(pose, Constants.constraints);
-            } else {
+            } else if (blackboard.getTargetPose("target") != null) {
+            }
+             else {
                 driveToCommand = null;
             }
     }
@@ -35,6 +41,8 @@ public class DriveToTargetPose extends BehaviorTreeNode {
     
         if (driveToCommand.isFinished()) {
             blackboard.set("isDone", true);
+            blackboard.set("hasCoral", false);
+            blackboard.set("hasAlgae", false);
             return ExecutionStatus.SUCCESS;
         }
 
