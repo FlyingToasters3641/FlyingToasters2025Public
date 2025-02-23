37302f14ba49b07042114bd10297771033f88641
Mars <74789790+TypicalMars@users.noreply.github.com>
2025-02-23 14:25:54 -0500

    small update to controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -220,12 +220,12 @@ public class RobotContainer {
         //Score coral commands
         
         controller.b().or(dashboard.L4()).onTrue(new ScoreL4(scorer, elevator));
-        controller.y().or(dashboard.L3()).onTrue(new ScoreL3(scorer, elevator));
+        controller.a().or(dashboard.L3()).onTrue(new ScoreL3(scorer, elevator));
         controller.x().or(dashboard.L2()).onTrue(new ScoreL2(scorer, elevator));
-        controller.a().or(dashboard.L1()).onTrue(new ScoreL1(scorer, elevator));
+        controller.povRight().or(dashboard.L1()).onTrue(new ScoreL1(scorer, elevator));
 
         //Score net
-        controller.rightBumper().or(dashboard.NET()).onTrue(new ScoreNet(scorer, elevator, intake, driveSimulation));
+        controller.y().or(dashboard.NET()).onTrue(new ScoreNet(scorer, elevator, intake, driveSimulation));
 
         //Intake algae
         controller.leftTrigger(0.1).onTrue(new IntakeGroundAlgaeV2(scorer, intake));
